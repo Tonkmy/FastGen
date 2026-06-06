2ef01c722968d442377149c125eaf7f5c717bb1a
Tonkmy <2991594241c@gmail.com>
2026-06-06 18:12:06 +0800

    Make EDM DMD2 8 GPU config self contained

--- fastgen/configs/experiments/EDM/config_dmd2_test_8gpu.py
+++ fastgen/configs/experiments/EDM/config_dmd2_test_8gpu.py
@@ -10,6 +10,9 @@ def create_config():
     # Eight-GPU DDP version of the EDM DMD2 test config.
     # Keep the same global batch size as config_dmd2_test.py: 8 GPUs * 8 samples/GPU = 64.
     config.trainer.ddp = True
+    config.trainer.max_iter = 5000
+    config.trainer.logging_iter = 50
+    config.trainer.save_ckpt_iter = 1000
     config.dataloader_train.batch_size = 8
     config.trainer.batch_size_global = 64
     config.log_config.name = "dmd2_test_8gpu"
