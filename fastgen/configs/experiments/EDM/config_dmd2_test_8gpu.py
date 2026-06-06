4911e3f8c60d617d29ad25a43a552ea4a3b975ab
Tonkmy <2991594241c@gmail.com>
2026-06-06 18:11:01 +0800

    Add EDM DMD2 8 GPU config

---
+++ fastgen/configs/experiments/EDM/config_dmd2_test_8gpu.py
@@ -0,0 +1,18 @@
+# SPDX-FileCopyrightText: Copyright (c) 2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+from fastgen.configs.experiments.EDM.config_dmd2_test import create_config as dmd2_test_create_config
+
+
+def create_config():
+    config = dmd2_test_create_config()
+
+    # Eight-GPU DDP version of the EDM DMD2 test config.
+    # Keep the same global batch size as config_dmd2_test.py: 8 GPUs * 8 samples/GPU = 64.
+    config.trainer.ddp = True
+    config.dataloader_train.batch_size = 8
+    config.trainer.batch_size_global = 64
+    config.log_config.name = "dmd2_test_8gpu"
+    config.log_config.wandb_mode = "disabled"
+
+    return config
--- fastgen/configs/experiments/EDM/run_dmd2_test_8gpu.sh
+++ fastgen/configs/experiments/EDM/run_dmd2_test_8gpu.sh
@@ -9,9 +9,4 @@ cd "$(git rev-parse --show-toplevel)"
 export CUDA_VISIBLE_DEVICES="${CUDA_VISIBLE_DEVICES:-0,1,2,3,4,5,6,7}"
 
 torchrun --standalone --nproc_per_node=8 train.py \
-  --config=fastgen/configs/experiments/EDM/config_dmd2_test.py \
-  - trainer.ddp=True \
-    dataloader_train.batch_size=8 \
-    trainer.batch_size_global=64 \
-    log_config.name=dmd2_test_8gpu \
-    log_config.wandb_mode=disabled
+  --config=fastgen/configs/experiments/EDM/config_dmd2_test_8gpu.py
