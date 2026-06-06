c49bf9eecddaccd0952d744ce989b052ff93c392
Tonkmy <2991594241c@gmail.com>
2026-06-06 17:44:23 +0800

    Add 8 GPU EDM DMD2 launch script

---
+++ fastgen/configs/experiments/EDM/run_dmd2_test_8gpu.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# SPDX-FileCopyrightText: Copyright (c) 2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+set -euo pipefail
+
+cd "$(git rev-parse --show-toplevel)"
+
+export CUDA_VISIBLE_DEVICES="${CUDA_VISIBLE_DEVICES:-0,1,2,3,4,5,6,7}"
+
+torchrun --standalone --nproc_per_node=8 train.py \
+  --config=fastgen/configs/experiments/EDM/config_dmd2_test.py \
+  - trainer.ddp=True \
+    dataloader_train.batch_size=8 \
+    trainer.batch_size_global=64 \
+    log_config.name=dmd2_test_8gpu \
+    log_config.wandb_mode=disabled
